b213270479a39ff11e70240dde6c5c16f387e52c
yuru1234 <and_drops326@yahoo.co.jp>
2018-11-07 22:35:07 +0900

    継承

---
+++ Iphone/src/Android.java
@@ -0,0 +1,15 @@
+
+public class Android {
+	public static void main(String[] args){
+		SmartPhone Android = new SmartPhone();
+		Android.play();
+		Android.stop();
+		Android.next();
+		Android.back();
+		Android.call();
+		Android.mail();
+		Android.photo();
+		Android.internet();
+	}
+
+}
